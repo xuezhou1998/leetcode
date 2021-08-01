b529e810582dd6f49be1c8a5e975ebe67d933f4d
xuezhou1998 <xuezhou1998@gmail.com>
2021-08-01 00:22:54 -0400

    using dynamic programming to solve

--- longestChain.py
+++ longestChain.py
@@ -1,347 +1,69 @@
-# class Solution:
-#     def longestStrChain(self, words: list) -> int:
-#         # chainLst=[]
-#         # pastSet=set()
-#         # for i in range(len(words)):
-#         #     pastSet.add(words[i])
-#         #     if chainLst==[]:
-#         #         chainLst.append([words[i]])
-#         wordsSorted=sorted(words,key=len)
-#         print(wordsSorted)
-#         firstLen=len(wordsSorted[0])
-#         maxChainLen=1
-#         for i in range(len(wordsSorted)):
-#             if len(wordsSorted[i])==firstLen:
-#                 pass
-#             else:
-#                 restLst=wordsSorted[i:]
-#                 break
-#         for i in range(len(wordsSorted)):
-#             if len(wordsSorted[i])==firstLen:
-#                 result=self.recrr(currWord=wordsSorted[i],sub_words=restLst,chainLen=1,wordLen=firstLen)
-#                 maxChainLen=max(maxChainLen,result)
-#             else:
-#                 break
-#         return maxChainLen
-#     def isDesc(self, predc:str,element:str)->bool:
 
-#         extra=0
-#         predcIdx=0
-#         print(len(predc),len(element))
-#         for i in range(len(element)):
-#             if element[i]==predc[predcIdx]:
-#                 if predcIdx==len(predc)-1:
-#                     break
-#                 predcIdx+=1
-#             elif extra==0:
-#                 extra+=1
-#             else:
-#                 return False
-            
         
-#         return True
-#     def recrr(self,currWord:str ,sub_words:list,chainLen:int,wordLen:int)->int:
-#         nextLst=[]
-#         restLst=[]
-#         for i in range(len(sub_words)):
-#             if len(sub_words[i])==wordLen+1:
-#                 nextLst.append(sub_words[i])
-#             else:
-#                 restLst=sub_words[i:]
-#                 break
-#         if len(nextLst)==0:
-#             return chainLen
-#         else:    
-#             maxCount=0
-#             for i in range(len(nextLst)):
-#                 if  self.isDesc(currWord,nextLst[i])==True:
-#                     result=self.recrr(nextLst[i],restLst,chainLen+1,wordLen+1)
-#                     maxCount=max(result,maxCount)
-#                 else:
-#                     return chainLen
-                    
-#             return maxCount
 
 class Solution:
     def longestStrChain(self, words: list) -> int:
-        # chainLst=[]
-        # pastSet=set()
-        # for i in range(len(words)):
-        #     pastSet.add(words[i])
-        #     if chainLst==[]:
-        #         chainLst.append([words[i]])
-        wordsSorted=sorted(words,key=len)
-        print(wordsSorted)
-        firstLen=len(wordsSorted[0])
-        maxChainLen=1
-        for i in range(len(wordsSorted)):
-            if len(wordsSorted[i])==firstLen:
-                pass
-            else:
-                restLst=wordsSorted[i:]
-                break
-        for i in range(len(wordsSorted)):
-            if len(wordsSorted[i])==firstLen:
-                result=self.recrr(currWord=wordsSorted[i],sub_words=restLst,chainLen=1,wordLen=firstLen)
-                maxChainLen=max(maxChainLen,result)
-            else:
-                break
-        return maxChainLen
-    def isDesc(self, predc:str,element:str)->bool:
+        
+        myMapIn={}
+        mySet=set(words)
+        for i in words:
+            myMapIn[i]=1
+        for i in words:
 
-        extra=0
-        predcIdx=0
-        print(len(predc),len(element))
-        for i in range(len(element)):
-            if element[i]==predc[predcIdx]:
-                if predcIdx==len(predc)-1:
-                    break
-                predcIdx+=1
-            elif extra==0:
-                extra+=1
+            myMapIn[i]=self.recrr(i,myMapIn,mySet)
+        return max(myMapIn.values())
+    def recrr(self, myWord:str,myMap: dict, mySet:set):
+
+        # print(myWord,myMap,mySet)
+        if len(myWord)==1:
+            return 1
+        myWordLst=list(myWord)
+        perm = self.myPerm(myWordLst)
+        perm =self.permuateDigits(list(perm),mySet)
+        # print(perm,'perm')
+        if len(perm)==0:
+            return 1
+        maxCount=1
+        for i in perm:
+            if myMap[i]>1:
+                # print(myWord,myMap,"branch_1")
+                maxCount=max(maxCount,1+myMap[i])
             else:
-                return False
+                # print(myWord,myMap,"branch_2")
+
+                interm=self.recrr(i,myMap,mySet)
+                myMap[i]=interm
+                maxCount=max(maxCount,1+interm)
+        return maxCount
+
+    def permuateDigits(self,perms:list,mySet:set)->list:
+        ret=[]
+        for i in perms:
+            # print("i",i)
+            joint="".join(i)
+            if joint in mySet:
+                ret.append(joint)
+        return ret
+    def myPerm(self, perms:list)->list:
+        ret=[]
+        for i in range(len(perms)):
+            elemt=perms.copy()
+            elemt[i]=""
             
-        
-        return True
-    def recrr(self,currWord:str ,sub_words:list,chainLen:int,wordLen:int)->int:
-        print(sub_words, "sub_words")
-        nextLst=[]
-        restLst=[]
-        for i in range(len(sub_words)):
-            if len(sub_words[i])==wordLen+1:
-                nextLst.append(sub_words[i])
-            else:
-                restLst=sub_words[i:]
-                break
-        if len(nextLst)==0:
-            return chainLen
-        else:    
-            maxCount=0
-            for i in range(len(nextLst)):
-                print(currWord,nextLst[i],"c,n")
-                if  self.isDesc(currWord,nextLst[i])==True:
-                    result=self.recrr(nextLst[i],restLst,chainLen+1,wordLen+1)
-                    maxCount=max(result,maxCount)
-                else:
-                    # return chainLen
-                    # pass
-                    maxCount=max(chainLen,maxCount)
-                    
-            return maxCount
-        
-        
 
-# flawed
-class Solution:
-    def longestStrChain(self, words: list) -> int:
-        # chainLst=[]
-        # pastSet=set()
-        # for i in range(len(words)):
-        #     pastSet.add(words[i])
-        #     if chainLst==[]:
-        #         chainLst.append([words[i]])
-        # wordsSorted=sorted(words,key=len)
-        wordsSorted=words
-        print(wordsSorted)
-        firstLen=len(wordsSorted[0])
-        lenDict={}
-        currLen=-1
-        for i in range(len(wordsSorted)):
-            if len(wordsSorted[i])==currLen:
-                if currLen not in lenDict.keys():
-                    lenDict[currLen]=[wordsSorted[i]]
-                else:
-                    lenDict[currLen].append(wordsSorted[i])
-            else:
-#                 if len(wordsSorted[i])==currLen+1:
-                    
-                currLen=len(wordsSorted[i])
-#                 else:
-#                     break
-                if currLen not in lenDict.keys():
-                    lenDict[currLen]=[wordsSorted[i]]
-                else:
-                    lenDict[currLen].append(wordsSorted[i])
-        # if currLen==firstLen:
-        #     return 1
-        maxChainLen=1
-        # for i in range(len(wordsSorted)):
-        #     if len(wordsSorted[i])==firstLen:
-        #         pass
-        #     else:
-        #         restLst=wordsSorted[i:]
-        #         break
-        result=1
-        firstLen=min(lenDict.keys())
-        for i in range(len(wordsSorted)):
-            if len(wordsSorted[i])==firstLen:
-                if firstLen+1 in lenDict.keys():
-                    
-                    result=self.recrr(myDict=lenDict,currWord=wordsSorted[i],sub_words=lenDict[firstLen+1],chainLen=1,wordLen=firstLen)
-                maxChainLen=max(maxChainLen,result)
-            else:
-                break
-        return maxChainLen
-    def isDesc(self, predc:str,element:str)->bool:
+            # print(elemt,"elemt",perms,"perms")
+            ret.append(elemt)
+        # print(ret,"ret")
+        return ret
 
-        extra=0
-        predcIdx=0
-        # print(len(predc),len(element))
-        for i in range(len(element)):
-            if element[i]==predc[predcIdx]:
-                if predcIdx==len(predc)-1:
-                    break
-                predcIdx+=1
-            elif extra==0:
-                extra+=1
-            else:
-                return False
-            
-        
-        return True
-    def recrr(self,myDict,currWord:str ,sub_words:list,chainLen:int,wordLen:int)->int:
-        print(sub_words,chainLen, "sub_words","chainLen")
-        nextLst=sub_words
-        if wordLen+2 in myDict.keys():
-            restLst=myDict[wordLen+2]
-        else:
-            restLst=[]
-        # for i in range(len(sub_words)):
-        #     if len(sub_words[i])==wordLen+1:
-        #         nextLst.append(sub_words[i])
-        #     else:
-        #         restLst=sub_words[i:]
-        #         break
-        if len(nextLst)==0:
-            return chainLen
-        else:    
-            maxCount=chainLen
-            for i in range(len(nextLst)):
-                print(currWord,nextLst[i],"c,n")
-                if  self.isDesc(currWord,nextLst[i])==True:
-                    result=self.recrr(myDict,nextLst[i],restLst,chainLen+1,wordLen+1)
-                    maxCount=max(result,maxCount)
-                else:
-                    # return chainLen
-                    # pass
-                    result=self.recrr(myDict,nextLst[i],restLst,1,wordLen+1)
-                    maxCount=max(result,maxCount)
-                    
-            return maxCount
-        
-        
-# flawed2
-class Solution:
-    def longestStrChain(self, words: list) -> int:
-        # chainLst=[]
-        # pastSet=set()
-        # for i in range(len(words)):
-        #     pastSet.add(words[i])
-        #     if chainLst==[]:
-        #         chainLst.append([words[i]])
-        # wordsSorted=sorted(words,key=len)
-        wordsSorted=words
-        # print(wordsSorted)
-        # firstLen=len(wordsSorted[0])
-        lenDict={}
-        currLen=-1
-        for i in range(len(wordsSorted)):
-            currLen=len(wordsSorted[i])
-            if currLen not in lenDict.keys():
-                lenDict[currLen]=[wordsSorted[i]]
-            else:
-                lenDict[currLen].append(wordsSorted[i])
-#             if len(wordsSorted[i])==currLen:
-#                 if currLen not in lenDict.keys():
-#                     lenDict[currLen]=[wordsSorted[i]]
-#                 else:
-#                     lenDict[currLen].append(wordsSorted[i])
-#             else:
-# #                 if len(wordsSorted[i])==currLen+1:
-                    
-#                 currLen=len(wordsSorted[i])
-# #                 else:
-# #                     break
-#                 if currLen not in lenDict.keys():
-#                     lenDict[currLen]=[wordsSorted[i]]
-#                 else:
-#                     lenDict[currLen].append(wordsSorted[i])
-        # if currLen==firstLen:
-        #     return 1
-        maxChainLen=1
-        # for i in range(len(wordsSorted)):
-        #     if len(wordsSorted[i])==firstLen:
-        #         pass
-        #     else:
-        #         restLst=wordsSorted[i:]
-        #         break
-        result=1
-        firstLen=min(lenDict.keys())
-        print(firstLen)
-        for i in range(len(lenDict[firstLen])):
-            if len(lenDict[firstLen][i])==firstLen:
-                if firstLen+1 in lenDict.keys():
-                    
-                    result=self.recrr(myDict=lenDict,currWord=lenDict[firstLen][i],sub_words=lenDict[firstLen+1],chainLen=1,wordLen=firstLen)
-                maxChainLen=max(maxChainLen,result)
-            else:
-                break
-        return maxChainLen
-    def isDesc(self, predc:str,element:str)->bool:
 
-        extra=0
-        predcIdx=0
-        # print(len(predc),len(element))
-        for i in range(len(element)):
-            if element[i]==predc[predcIdx]:
-                if predcIdx==len(predc)-1:
-                    break
-                predcIdx+=1
-            elif extra==0:
-                extra+=1
-            else:
-                return False
-            
-        
-        return True
-    def recrr(self,myDict,currWord:str ,sub_words:list,chainLen:int,wordLen:int)->int:
-        # print(sub_words,chainLen, "sub_words","chainLen")
-        nextLst=sub_words
-        if wordLen+2 in myDict.keys():
-            restLst=myDict[wordLen+2]
-        else:
-            restLst=[]
-        # for i in range(len(sub_words)):
-        #     if len(sub_words[i])==wordLen+1:
-        #         nextLst.append(sub_words[i])
-        #     else:
-        #         restLst=sub_words[i:]
-        #         break
-        if len(nextLst)==0:
-            return chainLen
-        else:    
-            maxCount=chainLen
-            for i in range(len(nextLst)):
-                # print(currWord,nextLst[i],"c,n")
-                if  self.isDesc(currWord,nextLst[i])==True:
-                    result=self.recrr(myDict,nextLst[i],restLst,chainLen+1,wordLen+1)
-                    maxCount=max(result,maxCount)
-                else:
-                    # return chainLen
-                    # pass
-                    result=self.recrr(myDict,nextLst[i],restLst,1,wordLen+1)
-                    maxCount=max(result,maxCount)
-                    
-            return maxCount
-        
-        
-class Solution:
-    def longestStrChain(self, words: list) -> int:
-        maxLen=0
+a=Solution()
+tests=[["a","b","ba","bca","bda","bdca"],
+["xbc","pcxbcf","xb","cxbc","pcxbc"],
+["abcd","dbqca"],["ksqvsyq","ks","kss","czvh","zczpzvdhx","zczpzvh","zczpzvhx","zcpzvh","zczvh","gr","grukmj","ksqvsq","gruj","kssq","ksqsq","grukkmj","grukj","zczpzfvdhx","gru"]]
+for i in tests:
+    print(a.longestStrChain(i))
 
-        return maxLen
-    def recrr()
 
 
 
---
+++ test.py
@@ -0,0 +1,9 @@
+from itertools import permutations
+ 
+# Get all permutations of length 2
+# and length 2
+perm = permutations([1, 2, 3], 2)
+print(list(perm))
+# Print the obtained permutations
+# for i in list(perm):
+#     print (i)
